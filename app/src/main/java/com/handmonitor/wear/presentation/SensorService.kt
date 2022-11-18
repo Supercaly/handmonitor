00c07f185fd90a57f1fc98de739b09714e2a8a07
Lorenzo Calisti <djonline96@gmail.com>
2022-11-18 11:54:10 +0100

    Change `SensorsService` to use the new `SensorsListenerThread` and `SensorsConsumer` logic.

--- app/src/main/java/com/handmonitor/wear/presentation/SensorService.kt
+++ app/src/main/java/com/handmonitor/wear/presentation/SensorService.kt
@@ -2,23 +2,30 @@ package com.handmonitor.wear.presentation
 
 import android.app.Service
 import android.content.Intent
-import android.os.Handler
-import android.os.HandlerThread
 import android.os.IBinder
 import android.util.Log
 
+/**
+ * Android Service to process sensor data.
+ *
+ * This Service has the task to collect the real-time data from
+ * the accelerometer and gyroscope inside the Android device and
+ * process them to obtain hand-washing and hand-rubbing related data.
+ */
 class SensorService : Service() {
     companion object {
         private const val TAG = "SensorService"
     }
 
-    private lateinit var mSensorListener: SensorListener
-    private lateinit var mPredictionRunnable: PredictionRunnable
     private lateinit var mSensorsData: SensorsData
-
-    private var mCollectionThread = HandlerThread("SensorsCollectionThread")
-    private lateinit var mPredictionThread: Thread
-    private lateinit var mCollectionThHd: Handler
+    private lateinit var mSensorsListenerThread: SensorsListenerThread
+    private lateinit var mConsumerThread: Thread
+    private lateinit var mSensorsConsumer: SensorsConsumerRn
+    private val mDo: SensorsConsumer = object : SensorsConsumer {
+        override fun onNewData(data: FloatArray) {
+            Log.d(TAG, "done: ciao ${data.size}")
+        }
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -26,29 +33,22 @@ class SensorService : Service() {
 
     override fun onCreate() {
         Log.i(TAG, "onCreate: Service created!")
-
         mSensorsData = SensorsData()
-
-        mCollectionThread.apply {
-            start()
-            mCollectionThHd = Handler(looper)
-        }
-        mSensorListener = SensorListener(this, mCollectionThHd, mSensorsData)
-        mPredictionRunnable = PredictionRunnable(mSensorsData)
-        mPredictionThread = Thread(mPredictionRunnable, "SensorsPredictionThread")
+        mSensorsListenerThread = SensorsListenerThread(this, mSensorsData)
+        mSensorsConsumer = SensorsConsumerRn(mSensorsData, mDo)
+        mConsumerThread = Thread(mSensorsConsumer, SensorsConsumerRn.threadName)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.i(TAG, "onStartCommand: Service started!")
-        mSensorListener.startListening()
-        mPredictionThread.start()
+        mSensorsListenerThread.startListening()
+        mConsumerThread.start()
         return START_STICKY
     }
 
     override fun onDestroy() {
         Log.i(TAG, "onDestroy: Service stopped!")
-        mSensorListener.stopListening()
-        mCollectionThread.quit()
-        mPredictionThread.interrupt()
+        mSensorsListenerThread.stopListening()
+        mConsumerThread.interrupt()
     }
 }
