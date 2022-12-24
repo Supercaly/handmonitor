86189fc62736dc459cc02d998793cac0c0228415
Lorenzo Calisti <djonline96@gmail.com>
2022-12-24 18:43:09 +0100

    Recorder: Add `RecorderService`
    This commit will add the `RecorderService` responsible for recording the actions
    data from the accelerometer and gyroscope sensors using `sensorslib`.
    
    The `RecorderService` is started ad stopped in the `RecorderRepository` depending
    on the UI state.

--- recorder/src/main/java/com/handmonitor/recorder/RecorderRepository.kt
+++ recorder/src/main/java/com/handmonitor/recorder/RecorderRepository.kt
@@ -5,13 +5,16 @@ import android.content.Intent
 import com.handmonitor.recorder.data.Action
 
 class RecorderRepository(private val mCtx: Context) {
+    private val mIntent by lazy { Intent(mCtx, RecorderService::class.java) }
     private var mRecordedAction: Action.Type? = null
 
     fun startRecording(action: Action.Type) {
         mRecordedAction = action
+        mCtx.startService(mIntent)
     }
 
     fun stopRecording() {
         mRecordedAction = null
+        mCtx.stopService(mIntent)
     }
 }
