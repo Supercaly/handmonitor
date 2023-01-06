45ab80cee16dea88baec0c86927bbba091bd9fc4
Lorenzo Calisti <djonline96@gmail.com>
2023-01-06 11:31:26 +0100

    Recorder: Fix `RecorderService` and `RecorderRepository`
    This commit will remove all service stuff from `RecorderRepository` leaving him
    fetching the actions from the database.
    
    This commit will add a binder to `RecorderService` so it can comunicate with the app
    to start/stop/save the recording process.

--- recorder/src/main/java/com/handmonitor/recorder/RecorderRepository.kt
+++ recorder/src/main/java/com/handmonitor/recorder/RecorderRepository.kt
@@ -1,20 +1,27 @@
 package com.handmonitor.recorder
 
-import android.content.Context
-import android.content.Intent
 import com.handmonitor.recorder.data.Action
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class RecorderRepository(private val mCtx: Context) {
-    private val mIntent by lazy { Intent(mCtx, RecorderService::class.java) }
-    private var mRecordedAction: Action.Type? = null
-
-    fun startRecording(action: Action.Type) {
-        mRecordedAction = action
-        mCtx.startService(mIntent)
+class RecorderRepository {
+    companion object {
+        private const val TAG = "RecorderRepository"
     }
 
-    fun stopRecording() {
-        mRecordedAction = null
-        mCtx.stopService(mIntent)
-    }
+    private val mActionsTime = MutableStateFlow(
+        mutableMapOf(
+            Action.Type.HandWash to Action.TimeRange(0.0f, 10.0f),
+            Action.Type.HandRub to Action.TimeRange(0.0f, 10.0f),
+            Action.Type.Eat to Action.TimeRange(0.0f, 10.0f),
+            Action.Type.TeethBrush to Action.TimeRange(0.0f, 10.0f),
+            Action.Type.FaceWash to Action.TimeRange(0.0f, 10.0f),
+            Action.Type.Write to Action.TimeRange(0.0f, 10.0f),
+            Action.Type.Type to Action.TimeRange(0.0f, 10.0f),
+            Action.Type.Housework to Action.TimeRange(0.0f, 10.0f),
+        )
+    )
+    val actionsTime: StateFlow<Map<Action.Type, Action.TimeRange>> =
+        mActionsTime.asStateFlow()
 }
