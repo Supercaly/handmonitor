2e36c53e897400fa21d28d90dff0e75594e21211
Lorenzo Calisti <djonline96@gmail.com>
2023-01-06 11:36:07 +0100

    Recorder: Fix UI and `RecorderViewModel`
    This commit is integrated with the last and changes the `RecorderViewModel`
    to manage the service in tandem with the main activity.
    
    In this commit the UI is also changed in different ways.

--- recorder/src/main/java/com/handmonitor/recorder/RecorderViewModel.kt
+++ recorder/src/main/java/com/handmonitor/recorder/RecorderViewModel.kt
@@ -1,31 +1,101 @@
 package com.handmonitor.recorder
 
+import android.content.ComponentName
+import android.content.ServiceConnection
+import android.os.IBinder
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.handmonitor.recorder.data.Action
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class RecorderViewModel(
-    private val mRecorderRepository: RecorderRepository
+    recorderRepository: RecorderRepository,
+    private val onStartService: (Action.Type) -> Unit
 ) : ViewModel() {
-    class RecorderViewModelFactory(private val repo: RecorderRepository) :
+    companion object {
+        private const val TAG = "RecorderViewModel"
+    }
+
+    class RecorderViewModelFactory(
+        private val repo: RecorderRepository,
+        private val onStartService: (Action.Type) -> Unit
+    ) :
         ViewModelProvider.NewInstanceFactory() {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T =
-            RecorderViewModel(repo) as T
+            RecorderViewModel(repo, onStartService) as T
     }
 
-    private val mIsRecording = MutableStateFlow(false)
-    val isRecording = mIsRecording.asStateFlow()
+    private var mJob: Job? = null
+
+    private var mRecorderBinder: RecorderService.RecorderBinder? = null
+    val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Log.d(TAG, "onServiceConnected: ")
+            mRecorderBinder = service as RecorderService.RecorderBinder
+            mShowRecordingScreen.value = true
+            if (mJob != null) {
+                mJob!!.cancel()
+            }
+            mJob = viewModelScope.launch {
+                mRecorderBinder!!.service.recordingTime.collect {
+                    mRecordingTime.value = it
+                }
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            Log.d(TAG, "onServiceDisconnected: ")
+            mRecorderBinder = null
+            //mShowRecordingScreen.value = false
+        }
+    }
+
+    private val mShowRecordingScreen =
+        MutableStateFlow(mRecorderBinder != null)
+    val showRecordingScreen: StateFlow<Boolean> =
+        mShowRecordingScreen.asStateFlow()
+
+    private val mRecordingTime = MutableStateFlow(1000L)
+    val recordingTimeString: StateFlow<String> =
+        mRecordingTime.map {
+            val m = ((it / 1_000) / 60).toInt()
+            val s = ((it / 1_000) % 60).toInt()
+            "%02d:%02d".format(m, s)
+        }.stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(),
+            "00:00"
+        )
+
+    val actionsTime: StateFlow<Map<Action.Type, Action.TimeRange>> =
+        recorderRepository.actionsTime
 
     fun startRecording(action: Action.Type) {
-        mIsRecording.value = true
-        mRecorderRepository.startRecording(action)
+        mShowRecordingScreen.value = true
+        onStartService(action)
     }
 
     fun stopRecording() {
-        mIsRecording.value = false
-        mRecorderRepository.stopRecording()
+        mRecorderBinder?.service?.stopRecording()
+    }
+
+    fun confirmSave() {
+        mShowRecordingScreen.value = false
+        mRecorderBinder?.service?.saveRecordedData()
+    }
+
+    fun discardSave() {
+        mShowRecordingScreen.value = false
+        mRecorderBinder?.service?.discardRecordedData()
     }
 }
