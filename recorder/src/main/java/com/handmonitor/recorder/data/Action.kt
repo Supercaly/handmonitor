03a24afd1af1cd03e71f2c6abb6a4c07deb1abd4
Lorenzo Calisti <djonline96@gmail.com>
2023-01-09 15:44:40 +0100

    Recorder: Update documentation of `recorder` app.

--- recorder/src/main/java/com/handmonitor/recorder/data/Action.kt
+++ recorder/src/main/java/com/handmonitor/recorder/data/Action.kt
@@ -1,6 +1,13 @@
 package com.handmonitor.recorder.data
 
+/**
+ * Represent an action.
+ */
 class Action {
+    /**
+     * Represent the type of action the can be recorded
+     * by the app.
+     */
     enum class Type {
         HandWash,
         HandRub,
@@ -12,5 +19,14 @@ class Action {
         Housework
     }
 
+    /**
+     * This class represent a time range of an action.
+     *
+     * An action recording has a fixed maximum duration and
+     * the current recorded duration.
+     *
+     * @property[remaining] Current recorded duration of this action type.
+     * @property[total] Total duration of this action type.
+     */
     class TimeRange(val remaining: Long, val total: Long = 600_000L)
 }
