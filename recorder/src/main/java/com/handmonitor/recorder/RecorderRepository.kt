152bf1921c96882a8856faebfc8966b3253ebf5c
Lorenzo Calisti <djonline96@gmail.com>
2023-01-09 14:39:13 +0100

    Recorder: Get action times from database
    This commit will change the `RecorderReporitory` to return a summary of the recorded
    action based on the real data stored on the device's database istead of some mock data.

--- recorder/src/main/java/com/handmonitor/recorder/RecorderRepository.kt
+++ recorder/src/main/java/com/handmonitor/recorder/RecorderRepository.kt
@@ -1,27 +1,72 @@
 package com.handmonitor.recorder
 
 import com.handmonitor.recorder.data.Action
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import com.handmonitor.recorder.database.AppDatabase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.mapLatest
 
-class RecorderRepository {
+class RecorderRepository(database: AppDatabase) {
     companion object {
         private const val TAG = "RecorderRepository"
     }
 
-    private val mActionsTime = MutableStateFlow(
-        mutableMapOf(
-            Action.Type.HandWash to Action.TimeRange(0.0f, 10.0f),
-            Action.Type.HandRub to Action.TimeRange(0.0f, 10.0f),
-            Action.Type.Eat to Action.TimeRange(0.0f, 10.0f),
-            Action.Type.TeethBrush to Action.TimeRange(0.0f, 10.0f),
-            Action.Type.FaceWash to Action.TimeRange(0.0f, 10.0f),
-            Action.Type.Write to Action.TimeRange(0.0f, 10.0f),
-            Action.Type.Type to Action.TimeRange(0.0f, 10.0f),
-            Action.Type.Housework to Action.TimeRange(0.0f, 10.0f),
-        )
-    )
-    val actionsTime: StateFlow<Map<Action.Type, Action.TimeRange>> =
-        mActionsTime.asStateFlow()
+    val actionsTime: Flow<Map<Action.Type, Action.TimeRange>> =
+        combine(
+            database.recordingDao().getRecordingsForAction(Action.Type.HandWash.name)
+                .mapLatest {
+                    it.fold(0L) { acc, rec ->
+                        acc + rec.durationMs
+                    }
+                },
+            database.recordingDao().getRecordingsForAction(Action.Type.HandRub.name).mapLatest {
+                it.fold(0L) { acc, rec ->
+                    acc + rec.durationMs
+                }
+            },
+            database.recordingDao().getRecordingsForAction(Action.Type.Eat.name).mapLatest {
+                it.fold(0L) { acc, rec ->
+                    acc + rec.durationMs
+                }
+            },
+            database.recordingDao().getRecordingsForAction(Action.Type.TeethBrush.name)
+                .mapLatest {
+                    it.fold(0L) { acc, rec ->
+                        acc + rec.durationMs
+                    }
+                },
+            database.recordingDao().getRecordingsForAction(Action.Type.FaceWash.name)
+                .mapLatest {
+                    it.fold(0L) { acc, rec ->
+                        acc + rec.durationMs
+                    }
+                },
+            database.recordingDao().getRecordingsForAction(Action.Type.Write.name).mapLatest {
+                it.fold(0L) { acc, rec ->
+                    acc + rec.durationMs
+                }
+            },
+            database.recordingDao().getRecordingsForAction(Action.Type.Type.name).mapLatest {
+                it.fold(0L) { acc, rec ->
+                    acc + rec.durationMs
+                }
+            },
+            database.recordingDao().getRecordingsForAction(Action.Type.Housework.name)
+                .mapLatest {
+                    it.fold(0L) { acc, rec ->
+                        acc + rec.durationMs
+                    }
+                }
+        ) { array ->
+            mapOf(
+                Action.Type.HandWash to Action.TimeRange(array[0]),
+                Action.Type.HandRub to Action.TimeRange(array[1]),
+                Action.Type.Eat to Action.TimeRange(array[2]),
+                Action.Type.TeethBrush to Action.TimeRange(array[3]),
+                Action.Type.FaceWash to Action.TimeRange(array[4]),
+                Action.Type.Write to Action.TimeRange(array[5]),
+                Action.Type.Type to Action.TimeRange(array[6]),
+                Action.Type.Housework to Action.TimeRange(array[7]),
+            )
+        }
 }
