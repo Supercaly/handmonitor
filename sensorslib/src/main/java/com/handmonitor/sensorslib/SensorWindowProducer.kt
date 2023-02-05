3fbcc7acb480605c56877411200f5675b15c49b0
Lorenzo Calisti <djonline96@gmail.com>
2023-02-05 18:28:23 +0100

    Sensorlib: Add `SensorWindowProducer` class
    This commit adds `SensorWindowProducer` class that helps collecting sensor data
    in a separate thread.

---
+++ sensorslib/src/main/java/com/handmonitor/sensorslib/SensorWindowProducer.kt
@@ -0,0 +1,161 @@
+package com.handmonitor.sensorslib
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import androidx.annotation.VisibleForTesting
+
+/**
+ * The class [SensorWindowProducer] implements the [SensorEventListener] callback
+ * and listens to events from the accelerometer and gyroscope sensors generating
+ * windows of sensor data.
+ *
+ * NOTE: This class collect the sensor data in his own thread that is managed
+ * by the class. Be aware of race conditions!
+ * @see[startSensors]
+ * @see[stopSensors]
+ */
+class SensorWindowProducer
+@VisibleForTesting
+internal constructor(
+    context: Context,
+    private val handlerThread: HandlerThread,
+    samplingMs: Long,
+    windowSize: Int,
+    private val onNewWindow: (SensorWindow) -> Unit,
+) : SensorEventListener {
+    companion object {
+        const val SENSOR_HANDLER_THREAD_NAME = "SensorWindowProducerThread"
+        private const val TAG = "SensorWindowProducer"
+    }
+
+    /**
+     * Creates a new instance of [SensorWindowProducer].
+     *
+     * @param[context] An instance of [Context] used to access the Android sensor service.
+     * @param[samplingMs] A [Long] value that specifies the sampling rate of the sensors.
+     * @param[windowSize] An [Int] value that specifies the size of the processing window.
+     * @param[onNewWindow] A callback that will be invoked when a new window of sensor data is produced.
+     * @throws[SensorNotSupportedException] If some sensor is not supported.
+     */
+    constructor(
+        context: Context,
+        samplingMs: Long,
+        windowSize: Int,
+        onNewWindow: (SensorWindow) -> Unit
+    ) : this(
+        context,
+        HandlerThread(SENSOR_HANDLER_THREAD_NAME),
+        samplingMs,
+        windowSize,
+        onNewWindow
+    )
+
+    // Time constants converted in microseconds
+    private val samplingPeriodUs: Int = samplingMs.msToUs().toInt()
+    private val maxLatencyUs: Int = (samplingMs * windowSize).msToUs().toInt()
+
+    // Sensor manager and sensors
+    private val mSensorManager: SensorManager
+    private var mAccSensor: Sensor? = null
+    private var mGyroSensor: Sensor? = null
+    private var mIsListening = false
+
+    // Sensor collection stuff
+    private val mWindowBuffer = SensorWindowBuffer(windowSize)
+    private val mAccFilter = SensorSampleFilter(samplingMs)
+    private val mGyroFilter = SensorSampleFilter(samplingMs)
+
+    init {
+        mSensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mAccSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        if (mAccSensor == null) {
+            throw SensorNotSupportedException(Sensor.TYPE_ACCELEROMETER)
+        }
+
+        mGyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        if (mGyroSensor == null) {
+            throw SensorNotSupportedException(Sensor.TYPE_GYROSCOPE)
+        }
+    }
+
+    /**
+     * Returns true if we are currently listening to sensors;
+     * false otherwise.
+     */
+    val isListening: Boolean
+        get() = mIsListening
+
+    /**
+     * This method starts listening to sensor data in a separate thread
+     * producing window of data and calling [onNewWindow] every time one
+     * is ready.
+     *
+     * @throws[IllegalThreadStateException] If the thread is started and was already running.
+     */
+    fun startSensors() {
+        if (mIsListening)
+            throw IllegalThreadStateException("thread is already running")
+
+        mIsListening = true
+        handlerThread.start()
+        val handler = Handler(handlerThread.looper)
+
+        mSensorManager.registerListener(
+            this,
+            mAccSensor,
+            samplingPeriodUs,
+            maxLatencyUs,
+            handler
+        )
+        mSensorManager.registerListener(
+            this,
+            mGyroSensor,
+            samplingPeriodUs,
+            maxLatencyUs,
+            handler
+        )
+        Log.d(TAG, "startSensors: start listening to sensors!")
+    }
+
+    /**
+     * This method stops listening to sensor data stopping the separate
+     * thread that handles them.
+     *
+     * When called without previously calling [startSensors] nothing
+     * will happen.
+     */
+    fun stopSensors() {
+        mIsListening = false
+        mSensorManager.unregisterListener(this)
+        handlerThread.quit()
+        Log.d(TAG, "stopSensors: stop listening to sensors!")
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        when (event?.sensor?.type) {
+            Sensor.TYPE_ACCELEROMETER -> {
+                if (mAccFilter.newSample(event) && mWindowBuffer.pushAccelerometer(event.values)) {
+                    onNewWindow(mWindowBuffer.window)
+                }
+            }
+            Sensor.TYPE_GYROSCOPE -> {
+                if (mGyroFilter.newSample(event) && mWindowBuffer.pushGyroscope(event.values)) {
+                    onNewWindow(mWindowBuffer.window)
+                }
+            }
+            else -> {
+                Log.w(TAG, "onSensorChanged: Unknown sensor type ${event?.sensor?.type}")
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.i(TAG, "onAccuracyChanged: Changed accuracy of sensor '${sensor?.name}' to '$accuracy'")
+    }
+}
