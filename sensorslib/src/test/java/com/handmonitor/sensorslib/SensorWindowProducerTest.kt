add5647e149af6745e77aefd2ca0fccaa6195688
Lorenzo Calisti <djonline96@gmail.com>
2023-02-05 19:19:41 +0100

    Sensorlib: Add more tests to `SensorWindowProducerTest`

--- sensorslib/src/test/java/com/handmonitor/sensorslib/SensorWindowProducerTest.kt
+++ sensorslib/src/test/java/com/handmonitor/sensorslib/SensorWindowProducerTest.kt
@@ -12,6 +12,7 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.just
+import io.mockk.mockkConstructor
 import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -25,7 +26,8 @@ class SensorWindowProducerTest {
         private const val windowSize = 200
     }
 
-    private val onNewWindow: (SensorWindow) -> Unit = {}
+    @MockK
+    private lateinit var mOnNewWindow: (SensorWindow) -> Unit
 
     @MockK
     private lateinit var mContext: Context
@@ -59,6 +61,11 @@ class SensorWindowProducerTest {
         every { mHandlerThread.start() } just Runs
         every { mHandlerThread.quit() } returns true
         every { mHandlerThread.looper } returns mLooper
+
+        every { mAccSensor.type } returns Sensor.TYPE_ACCELEROMETER
+        every { mGyroSensor.type } returns Sensor.TYPE_GYROSCOPE
+
+        every { mOnNewWindow.invoke(any()) } just Runs
     }
 
     @Test
@@ -70,7 +77,7 @@ class SensorWindowProducerTest {
             SensorWindowProducer(
                 mContext,
                 mHandlerThread,
-                samplingMs, windowSize, onNewWindow
+                samplingMs, windowSize, mOnNewWindow
             )
         }
 
@@ -81,7 +88,7 @@ class SensorWindowProducerTest {
             SensorWindowProducer(
                 mContext,
                 mHandlerThread,
-                samplingMs, windowSize, onNewWindow
+                samplingMs, windowSize, mOnNewWindow
             )
         }
     }
@@ -91,7 +98,7 @@ class SensorWindowProducerTest {
         val producer = SensorWindowProducer(
             mContext,
             mHandlerThread,
-            samplingMs, windowSize, onNewWindow
+            samplingMs, windowSize, mOnNewWindow
         )
         assertThat(producer.isListening).isFalse()
 
@@ -109,7 +116,7 @@ class SensorWindowProducerTest {
         val producer = SensorWindowProducer(
             mContext,
             mHandlerThread,
-            samplingMs, windowSize, onNewWindow
+            samplingMs, windowSize, mOnNewWindow
         )
         assertThat(producer.isListening).isFalse()
         producer.startSensors()
@@ -131,7 +138,7 @@ class SensorWindowProducerTest {
         val producer = SensorWindowProducer(
             mContext,
             mHandlerThread,
-            samplingMs, windowSize, onNewWindow
+            samplingMs, windowSize, mOnNewWindow
         )
         producer.startSensors()
         assertThat(producer.isListening).isTrue()
@@ -149,6 +156,91 @@ class SensorWindowProducerTest {
         verify {
             mSensorManager.unregisterListener(any<SensorEventListener>())
             mHandlerThread.quit()
+        }
+    }
+
+    @Test
+    fun `onSensorChanged refuse new samples`() {
+        mockkConstructor(SensorSampleFilter::class)
+        mockkConstructor(SensorWindowBuffer::class)
+        every { anyConstructed<SensorSampleFilter>().newSample(any()) } returns false
+        every { anyConstructed<SensorWindowBuffer>().pushAccelerometer(any()) } returns false
+        every { anyConstructed<SensorWindowBuffer>().pushGyroscope(any()) } returns false
+
+        val producer = SensorWindowProducer(
+            mContext,
+            mHandlerThread,
+            samplingMs, windowSize, mOnNewWindow
+        )
+        val accEvent = mockSensorEvent(mAccSensor)
+        val gyroEvent = mockSensorEvent(mGyroSensor)
+
+        producer.onSensorChanged(accEvent)
+        producer.onSensorChanged(gyroEvent)
+        verify {
+            anyConstructed<SensorSampleFilter>().newSample(accEvent)
+            anyConstructed<SensorSampleFilter>().newSample(gyroEvent)
+        }
+        verify(inverse = true) {
+            anyConstructed<SensorWindowBuffer>().pushAccelerometer(any())
+            anyConstructed<SensorWindowBuffer>().pushGyroscope(any())
+            mOnNewWindow.invoke(any())
+        }
+    }
+
+    @Test
+    fun `onSensorChanged accept new samples but not fill the window`() {
+        mockkConstructor(SensorSampleFilter::class)
+        mockkConstructor(SensorWindowBuffer::class)
+        every { anyConstructed<SensorSampleFilter>().newSample(any()) } returns true
+        every { anyConstructed<SensorWindowBuffer>().pushAccelerometer(any()) } returns false
+        every { anyConstructed<SensorWindowBuffer>().pushGyroscope(any()) } returns false
+
+        val producer = SensorWindowProducer(
+            mContext,
+            mHandlerThread,
+            samplingMs, windowSize, mOnNewWindow
+        )
+        val accEvent = mockSensorEvent(mAccSensor)
+        val gyroEvent = mockSensorEvent(mGyroSensor)
+
+        producer.onSensorChanged(accEvent)
+        producer.onSensorChanged(gyroEvent)
+        verify {
+            anyConstructed<SensorSampleFilter>().newSample(accEvent)
+            anyConstructed<SensorSampleFilter>().newSample(gyroEvent)
+            anyConstructed<SensorWindowBuffer>().pushAccelerometer(any())
+            anyConstructed<SensorWindowBuffer>().pushGyroscope(any())
+        }
+        verify(inverse = true) {
+            mOnNewWindow.invoke(any())
+        }
+    }
+
+    @Test
+    fun `onSensorChanged calls onNewWindow`() {
+        mockkConstructor(SensorSampleFilter::class)
+        mockkConstructor(SensorWindowBuffer::class)
+        every { anyConstructed<SensorSampleFilter>().newSample(any()) } returns true
+        every { anyConstructed<SensorWindowBuffer>().pushAccelerometer(any()) } returns true
+        every { anyConstructed<SensorWindowBuffer>().pushGyroscope(any()) } returns true
+
+        val producer = SensorWindowProducer(
+            mContext,
+            mHandlerThread,
+            samplingMs, windowSize, mOnNewWindow
+        )
+        val accEvent = mockSensorEvent(mAccSensor)
+        val gyroEvent = mockSensorEvent(mGyroSensor)
+
+        producer.onSensorChanged(accEvent)
+        producer.onSensorChanged(gyroEvent)
+        verify {
+            anyConstructed<SensorSampleFilter>().newSample(accEvent)
+            anyConstructed<SensorSampleFilter>().newSample(gyroEvent)
+            anyConstructed<SensorWindowBuffer>().pushAccelerometer(any())
+            anyConstructed<SensorWindowBuffer>().pushGyroscope(any())
+            mOnNewWindow.invoke(any())
         }
     }
 }
