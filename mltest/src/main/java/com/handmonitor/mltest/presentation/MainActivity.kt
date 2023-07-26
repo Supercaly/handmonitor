2a4ff2693541a328a7ea8517a8a41c1d4215ea2b
Lorenzo Calisti <djonline96@gmail.com>
2023-07-26 11:36:09 +0200

    Mltest: Fix `TestService` and `MainActivity`
    This commit fixes `TestService` and `MainActivity` used to test the sensorlib modules for the research.

--- mltest/src/main/java/com/handmonitor/mltest/presentation/MainActivity.kt
+++ mltest/src/main/java/com/handmonitor/mltest/presentation/MainActivity.kt
@@ -2,7 +2,6 @@ package com.handmonitor.mltest.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -12,6 +11,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -28,34 +31,37 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WearApp()
+            var serviceStarted by remember { mutableStateOf(false) }
+            HandMonitorTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background)
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    val context = LocalContext.current
+                    Text(text = "${TestService.version},${TestService.sampling},${TestService.size}")
+                    Button(
+                        onClick = {
+
+                            val intent = Intent(context, TestService::class.java)
+                            context.startForegroundService(intent)
+                            serviceStarted = true
+                        },
+                        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                    ) {
+                        Text(text = if (!serviceStarted) "Start" else "Started")
+                    }
+                }
+            }
         }
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
 
 @Composable
 fun WearApp() {
-    HandMonitorTheme {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            val context = LocalContext.current
-            Button(
-                onClick = {
-                    context.startService(Intent(context, TestService::class.java))
-                },
-                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
-            ) {
-                Text(text = "Start Test")
-            }
-        }
-    }
 }
 
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
