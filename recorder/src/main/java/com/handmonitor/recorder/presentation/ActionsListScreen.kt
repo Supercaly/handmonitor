f1191b361ef643280d293991458a45e5c12c11c5
Lorenzo Calisti <djonline96@gmail.com>
2023-10-14 10:35:08 +0200

    Use ScalingLazyColumn
    This commit uses ScalingLazyColumn instead of LazyColumn inside ActionsList to better work with round watch faces.

--- recorder/src/main/java/com/handmonitor/recorder/presentation/ActionsListScreen.kt
+++ recorder/src/main/java/com/handmonitor/recorder/presentation/ActionsListScreen.kt
@@ -2,11 +2,11 @@ package com.handmonitor.recorder.presentation
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import com.handmonitor.recorder.R
@@ -17,7 +17,8 @@ fun ActionsList(
     actionsTime: Map<Action.Type, Action.TimeRange>,
     onActionSelected: (action: Action.Type) -> Unit
 ) {
-    LazyColumn(
+
+    ScalingLazyColumn(
         modifier = Modifier.fillMaxSize()
     ) {
         item {
